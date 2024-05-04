c12b2000947cbe34e3b5cb3eeb6ee2e81d60bde6
Ys Desktop <handlecu@gmail.com>
2024-05-04 22:43:01 +0900

    block thread commit

--- projects/automated_warehouse/automated_warehouse.c
+++ projects/automated_warehouse/automated_warehouse.c
@@ -10,11 +10,18 @@
 
 #include "projects/automated_warehouse/aw_manager.h"
 
+#include "aw_message.h"
+
 struct robot* robots;
+int robot_num;
 
 // test code for central control node thread
 void test_cnt(){
+        boxes_from_central_control_node = malloc(sizeof(struct messsage_box) * robot_num);
+        boxes_from_robots = malloc(sizeof(struct message_box) * robot_num);
+
         while(1){
+                if(check_block_message())
                 print_map(robots, 4);
                 thread_sleep(1000);
                 block_thread();
@@ -31,6 +38,13 @@ void test_thread(void* aux){
         }
 }
 
+// thread 가 cnt 1, 2, 3 로봇 순서대로 진행되므로 cnt가 로봇 위치 읽으면서 어디로 갈지 정하고 쓰레도 차례대로 돌면 됨(thread_sleep)
+void cnt_thread(char *misson)
+{
+        while(1)
+        
+}
+
 // entry point of simulator
 void run_automated_warehouse(char **argv)
 {
@@ -39,19 +53,42 @@ void run_automated_warehouse(char **argv)
         printf("implement automated warehouse!\n");
 
         // test case robots
+        /*
         robots = malloc(sizeof(struct robot) * 4);
         setRobot(&robots[0], "R1", 5, 5, 0, 0);
         setRobot(&robots[1], "R2", 0, 2, 0, 0);
         setRobot(&robots[2], "R3", 1, 1, 1, 1);
         setRobot(&robots[3], "R4", 5, 5, 0, 0);
+        */
+        robot_num = atoi(argv[1]);
+        char _robotname[10];
+
+        robots = malloc(sizeof(struct robot) * atoi(robot_num));
+        for (int i=0; i<robot_num; i++)
+        {
+                fprintf(_robotname, "R%d", i+1);
+                setRobot(&robots[i], _robotname, 5, 5, 1, 0);
+        }
+
+       
 
         // example of create thread
+        /*
         tid_t* threads = malloc(sizeof(tid_t) * 4);
         int idxs[4] = {1, 2, 3, 4};
         threads[0] = thread_create("CNT", 0, &test_cnt, NULL);
         threads[1] = thread_create("R1", 0, &test_thread, &idxs[1]);
         threads[2] = thread_create("R2", 0, &test_thread, &idxs[2]);
         threads[3] = thread_create("R3", 0, &test_thread, &idxs[3]);
+        */
+
+        tid_t* threads = malloc(sizeof(tid_t) * (robot_num + 1));
+        threads[0] = thread_create("CNT", 0, &cnt_thread, &argv[2]);
+        for (int i=1; i<robot_num+1; i++)
+        {
+                fprintf(_robotname, "R%d", i);
+                threads[i] = thread_create(_robotname, i, &robot_thread, command);
+        }
 
         // if you want, you can use main thread as a central control node
         
--- projects/automated_warehouse/aw_thread.c
+++ projects/automated_warehouse/aw_thread.c
@@ -25,6 +25,7 @@ void block_thread(){
     // Code below is example
     enum intr_level old_level;
     old_level = intr_disable ();
+    list_push_back(&blocked_threads, running_thread());
     thread_block ();
     intr_set_level (old_level);
 }
@@ -33,6 +34,14 @@ void block_thread(){
  * A function unblocking all blocked threads in "blocked_threads" 
  * It must be called by central control thread
  */
-void unblock_threads(){
+void unblock_threads(){ 
+    enum intr_level old_level;
+
+    old_level = intr_disable();
+    while (!list_empty(block_thread))
+    {
+        thread_unblock(list_pop_front(block_thread));
+    }
+    intr_set_level(old_level);
     // you must implement this
 }
